47d4d0c8209aa5f4f112d844949fe6349b86f8a2
hhurst <hilary.hurst@sjsu.edu>
2023-01-19 12:44:37 -0800

    Updates to main.py to test upstream updating.

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ def print_hi(name):
     return
 
 
+def print_hi_twice(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}, Welcome to Physics 240.')
+    print(f'Hi, {name}, Welcome to Physics 240, again!')
+    return
+
+
 # Press the green button in the gutter to run the script in PyCharm.
 # Other IDEs might have a slightly different configuration.
 if __name__ == '__main__':
     print_hi('Hilary')
+    print_hi_twice('Hilary')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
