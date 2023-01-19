f50c7681b355c9246139d9a6711fa7312c6836a6
hhurst <hilary.hurst@sjsu.edu>
2023-01-19 12:02:55 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}, Welcome to Physics 240.')  # Press ⌘F8 to toggle the breakpoint.
+    return
+
+
+# Press the green button in the gutter to run the script in PyCharm.
+# Other IDEs might have a slightly different configuration.
+if __name__ == '__main__':
+    print_hi('Hilary')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
